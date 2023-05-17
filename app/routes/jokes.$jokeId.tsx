b364fd9aba87d488732bc5e05e377ed1d7dfc817
dubbha <dubbha@users.noreply.github.com>
2023-05-17 19:58:54 +0300

    optimistic ui

--- app/routes/jokes.$jokeId.tsx
+++ app/routes/jokes.$jokeId.tsx
@@ -5,14 +5,13 @@ import type {
 } from "@remix-run/node";
 import { redirect } from "@remix-run/node";
 import {
-  Form,
-  Link,
   useLoaderData,
   useParams,
   useRouteError,
   isRouteErrorResponse,
 } from "@remix-run/react";
 
+import { JokeDisplay } from "~/components/joke";
 import { db } from "~/utils/db.server";
 import { requireUserId, getUserId } from "~/utils/session.server";
 
@@ -77,23 +76,7 @@ export default function JokeRoute() {
   const data = useLoaderData<typeof loader>();
 
   return (
-    <div>
-      <p>Here's your hilarious joke:</p>
-      <p>{data.joke.content}</p>
-      <Link to=".">"{data.joke.name}" Permalink</Link>
-      {data.isOwner ? (
-        <Form method="post">
-          <button
-            className="button"
-            name="intent"
-            type="submit"
-            value="delete"
-          >
-            Delete
-          </button>
-        </Form>
-      ) : null}
-    </div>
+    <JokeDisplay isOwner={data.isOwner} joke={data.joke} />
   );
 }
 
